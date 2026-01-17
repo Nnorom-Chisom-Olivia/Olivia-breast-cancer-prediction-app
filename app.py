890e1ff17ae7e90a3dc0c49d917f7a903bf38b1a
Your Name <you@example.com>
2026-01-17 16:45:59 +0100

    Fix:clean up depencies

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
+import os
 from tensorflow import keras
 
 app = Flask(__name__)
@@ -59,4 +60,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
